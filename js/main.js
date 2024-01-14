4bac6acd639aeaa7690b0e8317fd1a9775b284ad
Bayode Olorundare <bayodeolorundare@gmail.com>
2024-01-14 00:30:31 -0500

    Update main.js
    Added additional functions for clearing, answer and calculation

--- js/main.js
+++ js/main.js
@@ -7,14 +7,27 @@ var button2 = document.getElementById('clear');
 
 button.onclick = function() {   
 	var div = document.getElementById('greeting');
-    if (div.style.display == 'none') {
-        div.style.display = 'block';
-    };
+	if (div.style.display == 'none') {
+		div.style.display = 'block';
+	};
 };
+
 button2.onclick = function() {   
 	var div = document.getElementById('greeting');
-    if (div.style.display == 'block') {
-        div.style.display = 'none';
-    };
+	if (div.style.display == 'block') {
+		div.style.display = 'none';
+	};
 };
+function clearAnswer() {
+	document.getElementById('answer').value = '';
+}
+
+function appendToAnswer(value) {
+	document.getElementById('answer').value += value;
+}
+
+function calculateResult() {
+	document.getElementById('answer').value = eval(document.getElementById('answer').value);
+}
+
 
